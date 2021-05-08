f2f71e683d8442290a1b624d96113363f27a4dd6
L1ndseyHerman <lherman21@jcu.edu>
2021-05-08 17:04:49 -0400

    Paragraph text changes when Button is pressed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-//import logo from './logo.svg';
-//import './App.css';
-//import React, { useState } from "react";
-//import styled from "styled-components";
+
 import React from "react";
 
-function clickMe() {
-  alert("You clicked me!");
+function clickMe() 
+{
+  document.querySelector("#thePar").textContent = "You clicked the button!";
 }
 
 function App() {
@@ -13,24 +11,9 @@ function App() {
     <>
       <div>
         <button onClick={clickMe}>Button</button>
+        <p id="thePar">Some text pls work</p>
       </div>
     </>
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
   );
 }
 
