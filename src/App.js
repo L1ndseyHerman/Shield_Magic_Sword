5179be37aa77f0741e07e653703ee5a02055ad00
L1ndseyHerman <lherman21@jcu.edu>
2021-05-08 19:45:18 -0400

    Made a second Par display a random number; 0, 1, or 2

--- src/App.js
+++ src/App.js
@@ -7,16 +7,26 @@ import React from "react";
 function chooseRock() 
 {
   document.querySelector("#thePar").textContent = "You clicked the rock button!";
+  chooseComputerRandomNumber();
 }
 
 function choosePaper() 
 {
   document.querySelector("#thePar").textContent = "You clicked the paper button!";
+  chooseComputerRandomNumber();
 }
 
 function chooseScissors() 
 {
   document.querySelector("#thePar").textContent = "You clicked the scissors button!";
+  chooseComputerRandomNumber();
+}
+
+function chooseComputerRandomNumber()
+{
+  //  There isn't an "int" datatype in JavaScript like there is in Java. Math.floor() makes it an int.
+  var compNumber = Math.floor(Math.random() * 3);  
+  document.querySelector("#compPar").textContent = compNumber;
 }
 
 function App() 
@@ -28,6 +38,7 @@ function App()
         <button onClick={choosePaper}>Paper</button>
         <button onClick={chooseScissors}>Scissors</button>
         <p id="thePar">Some text pls work</p>
+        <p id="compPar">Comp par</p>
       </div>
     </>
   );
