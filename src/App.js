aedfd4744f9a90762c4cb7e653a45ab155711de6
L1ndseyHerman <lherman21@jcu.edu>
2021-05-08 20:23:49 -0400

    Rock Paper Scissors JS done! Now just CSS

--- src/App.js
+++ src/App.js
@@ -6,27 +6,69 @@ import React from "react";
 
 function chooseRock() 
 {
-  document.querySelector("#thePar").textContent = "You clicked the rock button!";
-  chooseComputerRandomNumber();
+  var computerNumber = chooseComputerRandomNumber();
+  checkWinLooseTie(0, computerNumber);
 }
 
 function choosePaper() 
 {
-  document.querySelector("#thePar").textContent = "You clicked the paper button!";
-  chooseComputerRandomNumber();
+  var computerNumber = chooseComputerRandomNumber();
+  checkWinLooseTie(1, computerNumber);
 }
 
 function chooseScissors() 
 {
-  document.querySelector("#thePar").textContent = "You clicked the scissors button!";
-  chooseComputerRandomNumber();
+  var computerNumber = chooseComputerRandomNumber();
+  checkWinLooseTie(2, computerNumber);
 }
 
 function chooseComputerRandomNumber()
 {
   //  There isn't an "int" datatype in JavaScript like there is in Java. Math.floor() makes it an int.
-  var compNumber = Math.floor(Math.random() * 3);  
-  document.querySelector("#compPar").textContent = compNumber;
+  return Math.floor(Math.random() * 3);  
+}
+
+function checkWinLooseTie(playerChoiceNumber, computerChoiceNumber)
+{
+  //  0 = Rock, 1 = Paper, 2 = Scissors
+  if (playerChoiceNumber === 0 && computerChoiceNumber === 0)
+  {
+    document.querySelector("#thePar").textContent = "Player1 chose Rock and Computer chose Rock. Tie.";
+  }
+  else if (playerChoiceNumber === 0 && computerChoiceNumber === 1)
+  {
+    document.querySelector("#thePar").textContent = "Player1 chose Rock and Computer chose Paper. Computer wins!";
+  }
+  else if (playerChoiceNumber === 0 && computerChoiceNumber === 2)
+  {
+    document.querySelector("#thePar").textContent = "Player1 chose Rock and Computer chose Scissors. Player1 wins!";
+  }
+
+  else if (playerChoiceNumber === 1 && computerChoiceNumber === 0)
+  {
+    document.querySelector("#thePar").textContent = "Player1 chose Paper and Computer chose Rock. Player1 wins!";
+  }
+  else if (playerChoiceNumber === 1 && computerChoiceNumber === 1)
+  {
+    document.querySelector("#thePar").textContent = "Player1 chose Paper and Computer chose Paper. Tie.";
+  }
+  else if (playerChoiceNumber === 1 && computerChoiceNumber === 2)
+  {
+    document.querySelector("#thePar").textContent = "Player1 chose Paper and Computer chose Scissors. Computer wins!";
+  }
+
+  else if (playerChoiceNumber === 2 && computerChoiceNumber === 0)
+  {
+    document.querySelector("#thePar").textContent = "Player1 chose Scissors and Computer chose Rock. Computer wins!"
+  }
+  else if (playerChoiceNumber === 2 && computerChoiceNumber === 1)
+  {
+    document.querySelector("#thePar").textContent = "Player1 chose Scissors and Computer chose Paper. Player1 wins!"
+  }
+  else if (playerChoiceNumber === 2 && computerChoiceNumber === 2)
+  {
+    document.querySelector("#thePar").textContent = "Player1 chose Scissors and Computer chose Scissors. Tie."
+  }
 }
 
 function App() 
@@ -37,8 +79,7 @@ function App()
         <button onClick={chooseRock}>Rock</button>
         <button onClick={choosePaper}>Paper</button>
         <button onClick={chooseScissors}>Scissors</button>
-        <p id="thePar">Some text pls work</p>
-        <p id="compPar">Comp par</p>
+        <p id="thePar">Try to beat the computer at Rock-Paper-Scissors.</p>
       </div>
     </>
   );
