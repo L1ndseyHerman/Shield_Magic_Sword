d3cc3dbfa7ec1fc7ca06df311337eb01d58c65d4
L1ndseyHerman <lherman21@jcu.edu>
2021-05-10 00:11:09 -0400

    Should the js be in App.js or index.js? Here's index.js

--- src/App.js
+++ src/App.js
@@ -1,90 +1,11 @@
 import './App.css';
 import React from "react";
 
-//  This doesn't appear. Why not? 
-//<p>A static paragraph.</p>
+//  THIS CODE IS NO LONGER IN USE! I tested regular JavaScript here before turning it into React JS in the index.js
 
-function chooseRock() 
-{
-  var computerNumber = chooseComputerRandomNumber();
-  checkWinLooseTie(0, computerNumber);
-}
-
-function choosePaper() 
-{
-  var computerNumber = chooseComputerRandomNumber();
-  checkWinLooseTie(1, computerNumber);
-}
-
-function chooseScissors() 
-{
-  var computerNumber = chooseComputerRandomNumber();
-  checkWinLooseTie(2, computerNumber);
-}
-
-function chooseComputerRandomNumber()
-{
-  //  There isn't an "int" datatype in JavaScript like there is in Java. Math.floor() makes it an int.
-  return Math.floor(Math.random() * 3);  
-}
-
-function checkWinLooseTie(playerChoiceNumber, computerChoiceNumber)
-{
-  //  0 = Rock, 1 = Paper, 2 = Scissors
-  if (playerChoiceNumber === 0 && computerChoiceNumber === 0)
-  {
-    document.querySelector("#thePar").textContent = "Player1 chose Rock and Computer chose Rock. Tie.";
-  }
-  else if (playerChoiceNumber === 0 && computerChoiceNumber === 1)
-  {
-    document.querySelector("#thePar").textContent = "Player1 chose Rock and Computer chose Paper. Computer wins!";
-  }
-  else if (playerChoiceNumber === 0 && computerChoiceNumber === 2)
-  {
-    document.querySelector("#thePar").textContent = "Player1 chose Rock and Computer chose Scissors. Player1 wins!";
-  }
-
-  else if (playerChoiceNumber === 1 && computerChoiceNumber === 0)
-  {
-    document.querySelector("#thePar").textContent = "Player1 chose Paper and Computer chose Rock. Player1 wins!";
-  }
-  else if (playerChoiceNumber === 1 && computerChoiceNumber === 1)
-  {
-    document.querySelector("#thePar").textContent = "Player1 chose Paper and Computer chose Paper. Tie.";
-  }
-  else if (playerChoiceNumber === 1 && computerChoiceNumber === 2)
-  {
-    document.querySelector("#thePar").textContent = "Player1 chose Paper and Computer chose Scissors. Computer wins!";
-  }
-
-  else if (playerChoiceNumber === 2 && computerChoiceNumber === 0)
-  {
-    document.querySelector("#thePar").textContent = "Player1 chose Scissors and Computer chose Rock. Computer wins!"
-  }
-  else if (playerChoiceNumber === 2 && computerChoiceNumber === 1)
-  {
-    document.querySelector("#thePar").textContent = "Player1 chose Scissors and Computer chose Paper. Player1 wins!"
-  }
-  else if (playerChoiceNumber === 2 && computerChoiceNumber === 2)
-  {
-    document.querySelector("#thePar").textContent = "Player1 chose Scissors and Computer chose Scissors. Tie."
-  }
-}
-
-function App() 
-{
+function App() {
   return (
-    <body>
-      <main>
-        <h1>Rock, Paper, Scissors</h1>
-        <button onClick={chooseRock}>Rock</button>
-        <button onClick={choosePaper}>Paper</button>
-        <button onClick={chooseScissors}>Scissors</button>
-        <p id="thePar">Try to beat the computer at Rock-Paper-Scissors.</p>
-        <p>And check out <a id="aLink1" href="https://l1ndseyherman.github.io/">My Other Website</a>.</p>
-        <footer id="theFooter">Current PWA version is 1.0.0, updated 05/09/21</footer>
-      </main>
-    </body>
+    <p>An extra paragraph.</p>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 
-//  This doesn't appear at all. 
-//<p>A static paragraph.</p>
+class FirstLoad extends React.Component
+{
+    render() 
+    {
+        return (
+            <div>
+                <h1>Rock, Paper, Scissors</h1>
+                <p id="thePar">Try to beat the computer at Rock-Paper-Scissors.</p>
+                <p>And check out <a id="aLink1" href="https://l1ndseyherman.github.io/">My Other Website</a>.</p>
+                <footer id="theFooter">Current PWA version is 1.0.0, updated 05/09/21</footer>
+            </div>
+        );
+    }
+}
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<FirstLoad></FirstLoad>, document.getElementById('root'));
 
-//  This also doesn't appear at all. 
-//<p>A static paragraph.</p>
+//ReactDOM.render(<App />, document.getElementById('root'));
