abe61cc2de020071d6e0b5be2ec9f0669493516c
L1ndseyHerman <lherman21@jcu.edu>
2021-05-10 01:12:54 -0400

    Back to the first way, does this only re-render the par?

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 
-class FirstLoad extends React.Component
-{
-    render() 
-    {
-        return (
-            <div>
-                <h1>Rock, Paper, Scissors</h1>
-                <p id="thePar">Try to beat the computer at Rock-Paper-Scissors.</p>
-                <p>And check out <a id="aLink1" href="https://l1ndseyherman.github.io/">My Other Website</a>.</p>
-                <footer id="theFooter">Current PWA version is 1.0.0, updated 05/09/21</footer>
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<FirstLoad></FirstLoad>, document.getElementById('root'));
-
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
